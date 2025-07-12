b0b3d3fa214f17a21d388b70b36b0464fbb04765
flavioleonard <115761732+flavioleonard@users.noreply.github.com>
2025-07-12 01:08:47 -0300

    refactor: usando implementação dos slides

--- enderecamento_aberto.c
+++ enderecamento_aberto.c
@@ -1,155 +1,177 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 
 #define TOTAL_REGISTROS 10000
 #define TAMANHO_TABELA 100000
 #define TAM_NOME 50
 
-typedef struct Registro
-{
+typedef struct Registro {
     char nome[TAM_NOME];
     long long int cpf;
     int nota;
 } TRegistro;
 
-typedef struct RegistroTabela
-{
-    TRegistro registro;
-    struct RegistroTabela *proximo;
-    bool ocupado;
-} TRegistroTabela;
+typedef TRegistro* Hash;
 
-int calcularHash(long long chaveCPF)
-{   
-    int k;
-    int nove_digitos = chaveCPF / 100;
-    
-    srand((unsigned int)nove_digitos);
-    int hash = rand();
-    hash = (hash + k) % TAMANHO_TABELA;
-
-    return hash;
+int hash(long long cpf, int m, int k) {
+    unsigned int chave = (unsigned int)(cpf / 100); // nove primeiros dígitos
+    srand(chave); // reinicia com base no CPF
+    int valor_hash = rand(); // gera sempre o mesmo valor para o mesmo CPF
+    return (valor_hash + k) % m;
 }
 
 
-TRegistroTabela *inicializa_tabela()
-{
-    TRegistroTabela *tabela = malloc(sizeof(TRegistroTabela) * TAMANHO_TABELA);
-    if (tabela == NULL)
-        return NULL;
+int busca(Hash *tab, int m, long long cpf, int *achou) {
+    *achou = 0;
+    int end = -1;
+    int pos_livre = -1;
+    int k = 0;
 
-    for (int i = 0; i < TAMANHO_TABELA; i++)
-    {
-        tabela[i].ocupado = false;
-        tabela[i].proximo = NULL;
-    }
-
-    return tabela;
-}
-
-void inserir_na_tabela(TRegistroTabela *tabela, TRegistro registro)
-{
-    int hash = calcularHash(registro.cpf);
-    int posicao = hash;
-
-    while (tabela[posicao].ocupado)
-    {
-        posicao = (posicao + 1) % TAMANHO_TABELA;
-        if (posicao == hash)
-        {
-            printf("Tabela cheia! Não é possível inserir o registro de CPF %lld\n", registro.cpf);
-            return;
+    while (k < m) {
+        end = hash(cpf, m, k);
+        if (tab[end] != NULL && tab[end]->cpf == cpf) {
+            *achou = 1;
+            break;
+        } else if (tab[end] == NULL && pos_livre == -1) {
+            pos_livre = end;
         }
+        k++;
     }
 
-    tabela[posicao].registro = registro;
-    tabela[posicao].ocupado = true;
-    tabela[posicao].proximo = NULL;
+    return *achou ? end : pos_livre;
+}
 
-    if (posicao != hash)
-    {
-        int i = hash;
-        while (true)
-        {
-            if (tabela[i].ocupado && (tabela[i].registro.cpf / 100 == registro.cpf / 100))
-            {
-                tabela[i].proximo = &tabela[posicao];
-                break;
-            }
-            i = (i + 1) % TAMANHO_TABELA;
-            if (i == posicao)
-                break;
-        }
+TRegistro* busca_registro(Hash *tab, int m, long long cpf) {
+    int achou;
+    int pos = busca(tab, m, cpf, &achou);
+
+    if (achou) {
+        return tab[pos];  
+    } else {
+        return NULL;      
     }
 }
 
-void carregar_e_inserir(TRegistroTabela *tabela)
-{
+
+TRegistro *aloca_registro(char *nome, long long cpf, int nota) {
+    TRegistro *novo = malloc(sizeof(TRegistro));
+    if (novo) {
+        strcpy(novo->nome, nome);
+        novo->cpf = cpf;
+        novo->nota = nota;
+    }
+    return novo;
+}
+
+void insere(Hash *tab, int m, char *nome, long long cpf, int nota) {
+    int achou;
+    int end = busca(tab, m, cpf, &achou);
+
+    if (!achou) {
+        if (end != -1) {
+            tab[end] = aloca_registro(nome, cpf, nota);
+        } else {
+            printf("Overflow. Não foi possível inserir CPF %lld\n", cpf);
+        }
+    } else {
+        printf("CPF já existe. Inserção inválida!\n");
+    }
+}
+
+void exclui(Hash *tab, int m, long long cpf) {
+    int achou;
+    int end = busca(tab, m, cpf, &achou);
+    if (achou) {
+        free(tab[end]);
+        tab[end] = NULL;
+    } else {
+        printf("CPF %lld não encontrado. Remoção não realizada!\n", cpf);
+    }
+}
+
+void inicializa(Hash *tab, int m) {
+    for (int i = 0; i < m; i++) {
+        tab[i] = NULL;
+    }
+}
+
+void carrega_registros_na_tabela(Hash *tabela) {
     FILE *arquivo = fopen("registros.dat", "rb");
-    if (arquivo == NULL)
-    {
-        perror("Erro ao abrir o arquivo");
+    if (arquivo == NULL) {
+        perror("Erro ao abrir arquivo");
         exit(1);
     }
 
     TRegistro reg;
-    int contador = 0;
-
-    while (fread(&reg, sizeof(TRegistro), 1, arquivo) == 1)
-    {
-        inserir_na_tabela(tabela, reg);
-        contador++;
-        if (contador % 1000 == 0)
-            printf("%d registros inseridos...\n", contador);
+    while (fread(&reg, sizeof(TRegistro), 1, arquivo) == 1) {
+        insere(tabela, TAMANHO_TABELA, reg.nome, reg.cpf, reg.nota);
     }
 
     fclose(arquivo);
-    printf("Inserção concluída: %d registros.\n", contador);
 }
 
-void exibir_tabela(TRegistroTabela *tabela)
-{
-    printf("\n--- TABELA HASH ---\n");
-    for (int i = 0; i < TAMANHO_TABELA; i++)
-    {
-        if (tabela[i].ocupado)
-        {
-            printf("Proximo %d | CPF: %lld | Nota: %d | Nome: %.*s\n", i,
-                   tabela[i].registro.cpf,
-                   tabela[i].registro.nota,
-                   TAM_NOME, tabela[i].registro.nome);
-            if (tabela[i].proximo != NULL)
-            {
-                printf("    -> Encadeado com CPF: %lld\n", tabela[i].proximo->registro.cpf);
-            }
+
+void imprimir_tabela(Hash *tabela) {
+    for (int i = 0; i < TAMANHO_TABELA; i++) {
+        if (tabela[i] != NULL) {
+            printf("Posicao %d | CPF: %lld | Nome: %s | Nota: %d\n",
+                   i,
+                   tabela[i]->cpf,
+                   tabela[i]->nome,
+                   tabela[i]->nota);
         }
     }
 }
 
+void liberar_tabela(Hash *tabela) {
+    for (int i = 0; i < TAMANHO_TABELA; i++) {
+        if (tabela[i] != NULL) {
+            free(tabela[i]);
+        }
+    }
+}
 
-int main()
-{
-    TRegistroTabela *tabela = inicializa_tabela();
-    if (tabela == NULL)
-    {
-        perror("Erro ao alocar memória para a tabela");
+int main() {
+    Hash *tabela = malloc(sizeof(Hash) * TAMANHO_TABELA);
+    if (!tabela) {
+        perror("Erro de alocação");
         return 1;
     }
 
-    //carregar_e_inserir(tabela); 
+    inicializa(tabela, TAMANHO_TABELA);
 
-    ///exibir_tabela(tabela); 
+    carrega_registros_na_tabela(tabela);
 
-    int hash = calcularHash(123456789);
-    printf("%d\n", hash);
-    int hash2 = calcularHash(987654321);
-    printf("%d\n", hash2);
+    long long cpf_alvo = 5841347206LL;
+    int achou;
+    int pos = busca(tabela, TAMANHO_TABELA, cpf_alvo, &achou);
 
+    if (achou) {
+        printf("\nRegistro encontrado na posição %d:\n", pos);
+        printf("CPF: %lld | Nome: %s | Nota: %d\n",
+               tabela[pos]->cpf,
+               tabela[pos]->nome,
+               tabela[pos]->nota);
+    } else {
+        printf("\nCPF %lld não encontrado na tabela.\n", cpf_alvo);
+    }
 
+    int busc = busca(tabela, TAMANHO_TABELA, cpf_alvo, &achou);
 
+    if (achou) {
+        printf("\nRegistro encontrado na posição %d:\n", pos);
+        printf("CPF: %lld | Nome: %s | Nota: %d\n",
+               tabela[pos]->cpf,
+               tabela[pos]->nome,
+               tabela[pos]->nota);
+    } else {
+        printf("\nCPF %lld não encontrado na tabela.\n", cpf_alvo);
+    }
 
+    liberar_tabela(tabela);
     free(tabela);
     return 0;
 }
+
+
